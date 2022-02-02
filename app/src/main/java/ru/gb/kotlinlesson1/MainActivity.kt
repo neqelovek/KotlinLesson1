ac5b55463bdb00a1996be07733a4ccbb5912e8bd
neqelovek <neqelovek@gmail.com>
2022-02-02 20:29:04 +0300

    Java -> Kotlin

---
+++ app/src/main/java/ru/gb/kotlinlesson1/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.gb.kotlinlesson1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.gb.kotlinlesson1.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/ru/gb/kotlinlesson1/ExampleUnitTest.kt
@@ -0,0 +1,19 @@
+package ru.gb.kotlinlesson1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.junit.Assert
+import org.junit.Test
+import ru.gb.kotlinlesson1.R
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, (2 + 2).toLong())
+    }
+}
